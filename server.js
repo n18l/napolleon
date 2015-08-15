0335864f88588358541cc50a209905150c691f0a
Nick Brombal <nick@commercehouse.com>
2015-08-14 19:42:20 -0500

    Testing slack slash command responses

--- server.js
+++ server.js
@@ -25,15 +25,13 @@ router.get('/napolleon', function(req, res) {
     // if (req.query.token != '4ofROgiGBbMVk1ibnDOflQVU')
     //     return;
 
-    var userChoices       = req.query.text.split(' -');
-    var userQuestion      = userChoices.shift();
-    var userChannelID     = req.query.channel_id;
-    var userChannelPrefix = userChannelID[0] == 'D' ? '@' : '#';
-    var userChannelName   = userChannelPrefix + req.query.channel_name;
-    var pollAnnouncement  = req.query.user_name + ' asks: "' + userQuestion + '"';
-    var slackToken        = "xoxp-3148856461-3909050702-9148379030-c5d7d2";
-
-    res.json({message:userChannelID});
+    var userChoices      = req.query.text.split(' -');
+    var userQuestion     = userChoices.shift();
+    var userChannelID    = req.query.channel_id;
+    var userChannelType  = req.query.channel_name == 'directmessage' ? 'im' : ( req.query.channel_name == 'privategroup' ? 'groups' : 'channels');
+    var userChannelName  = userChannelType == 'im' ? '@' + req.query.user_name : '#' + req.query.channel_name;
+    var pollAnnouncement = req.query.user_name + ' asks: "' + userQuestion + '"';
+    var slackToken       = "xoxp-3148856461-3909050702-9148379030-c5d7d2";
 
     // Define which emoji to use for options
     var optionEmoji = ['one','two','three','four','five','six','seven','eight','nine'];
@@ -50,7 +48,7 @@ router.get('/napolleon', function(req, res) {
     // Function to get recent channel history and find the poll's timestamp
     var getHistory = function() {
         request.get({
-            url: 'https://slack.com/api/' + (userChannelID[0]) + '.history',
+            url: 'https://slack.com/api/' + userChannelType + '.history',
             qs: {
                 "token":   slackToken,
                 "channel": userChannelID,
@@ -58,7 +56,8 @@ router.get('/napolleon', function(req, res) {
             }
         }, function (error, response, body) {
             if (!error && response.statusCode == 200) {
-                var lastMessage = _.findWhere(JSON.parse(body).messages, {"bot_id": "B0943D5MX"})
+                console.log(body);
+                var lastMessage   = _.findWhere(JSON.parse(body).messages, {"bot_id": "B0943D5MX"});
                 var lastTimestamp = lastMessage.ts;
 
                 _.each(userChoices.reverse(), function(element, index, list) {
@@ -79,46 +78,58 @@ router.get('/napolleon', function(req, res) {
                 "timestamp": timestamp
             }
         }, function (error, response, body) {
-            console.log(body);
+            if (!error && response.statusCode == 200) {
+                console.log(body);
+            }
         });
     };
 
     // Function to post poll to Slack
-    request.post({
-        url: 'https://hooks.slack.com/services/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', 
-        json: {
-            "channel": userChannelName,
-            "attachments":[{
-                "fallback": pollAnnouncement,
-                "pretext":  pollAnnouncement,
-                "color":    "#E5E5E5",
-                "fields":[{
-                    "value": pollChoices,
-                    "short": false
+    var postToSlack = function(channel) {
+        request.post({
+            url: 'https://hooks.slack.com/services/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', 
+            json: {
+                "channel": channel,
+                "attachments":[{
+                    "fallback": pollAnnouncement,
+                    "pretext":  pollAnnouncement,
+                    "color":    "blue",
+                    "fields":[{
+                        "value": pollChoices,
+                        "short": false
+                    }]
                 }]
-            }]
-        }
-    }, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            console.log(body, userChannelName);
-            getHistory();
-        }
-    });
+            }
+        }, function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                console.log(body);
+                getHistory();
+            }
+        });
+    };
 
     // Determine if user is posting to a Channel or Direct Message
-    // request.get({
-    //     url: 'https://slack.com/api/channels.list',
-    //     qs: {
-    //         "token": slackToken,
-    //         "exclude_archived": 1
-    //     }
-    // }, function(error, response, body) {
-    //     if (!error && response.statusCode == 200) {
-    //         var postingToChannel = _.findWhere(JSON.parse(body).channels, {"id": userChannelID});
-    //         userChannelPrefix = postingToChannel ? '#' : '@';
-    //         postToSlack();
-    //     }
-    // });
+    var getPrivateGroupName = function() {
+        request.get({
+            url: 'https://slack.com/api/groups.list',
+            qs: {
+                "token": slackToken,
+                "exclude_archived": 1
+            }
+        }, function(error, response, body) {
+            if (!error && response.statusCode == 200) {
+                console.log(body);
+                var privateGroup = _.findWhere(JSON.parse(body).groups, {"id": userChannelID});
+                postToSlack(privateGroup.name);
+            }
+        });
+    };
+
+    if (userChannelType == 'groups')
+        getPrivateGroupName();
+    else
+        postToSlack(userChannelName);
+
 });
 
 // REGISTER ROUTES ---------------------------------------------
